5270d13543ba1a687ce79e5ac5e7646542dfc74e
Alice Moraes <alicegamer35@gmail.com>
2026-09-21 17:01:41 -0300

    chore: atualiza algumas linhas

--- aula08-09-metodo-get-post-put-patch-delete/src/convidados.controller.ts
+++ aula08-09-metodo-get-post-put-patch-delete/src/convidados.controller.ts
@@ -27,7 +27,7 @@ export class ConvidadosController {
         }
     }
 
-    @Patch()
+    @Patch(':id')
     atualizarIdade(@Param('id') id: number, @Body('idade') idade: number) {
         console.log(`[ADMINISTRADOR]: Atualizando a idade do ID ${id}`);
         return this.convidadosService.atualizarIdade(+id, idade);
